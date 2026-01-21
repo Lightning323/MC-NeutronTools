ce2ca81dd96bc6b1a23359735535f748fe126236
zipCoder933 <sam.p.w524@gmail.com>
2026-01-20 18:19:45 -0700

    fix

--- src/main/java/org/zipcoder/neutrontools/events/TagEventHandler.java
+++ src/main/java/org/zipcoder/neutrontools/events/TagEventHandler.java
@@ -2,13 +2,16 @@ package org.zipcoder.neutrontools.events;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.zipcoder.neutrontools.NeutronTools;
 import org.zipcoder.neutrontools.creativetabs.CreativeTabs;
 import org.zipcoder.neutrontools.creativetabs.client.impl.CreativeModeTabMixin_I;
+
 
 @Mod.EventBusSubscriber(modid = NeutronTools.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class TagEventHandler {
@@ -18,8 +21,10 @@ public class TagEventHandler {
         // This check ensures we only update the creative tabs when Item tags are updated,
         // since we need them in order to determine which items go in which tabs
         if (event.getRegistryAccess().registry(Registries.ITEM).isPresent()) {
-            NeutronTools.LOGGER.info("Item tags have been registered and are now bound! Reloading creative tabs");
-            CreativeTabs.reloadTabs();
+            if (FMLEnvironment.dist == Dist.CLIENT) {
+                NeutronTools.LOGGER.info("Item tags have been registered and are now bound! Reloading creative tabs");
+                CreativeTabs.reloadTabs();
+            }
         }
     }
 }
